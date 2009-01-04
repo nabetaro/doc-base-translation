2e5ea90fd8a962c54dd61eba239243ae41f9464c
Robert Luberda <robert@debian.org>
2009-01-04 17:10:53 +0100

    Build system improvements.

--- Makefile
+++ Makefile
@@ -1,46 +1,46 @@
 # vim:ts=2
 # makefile for doc-base
-# $Id: Makefile 167 2009-01-04 12:24:45Z robert $
+# $Id: Makefile 168 2009-01-04 16:10:53Z robert $
 #
 
-ALL_TARGET 		:= build-local
-AFTER_INSTALL	:= $(call check_install)
-SUBDIRS		 		:= doc data perl po
-include 			common.mk
+ALL_TARGET    := build-local
+AFTER_INSTALL := $(call check_install)
+SUBDIRS       := doc data perl po
+include       common.mk
 
 x:=$(subst /, ,$(abspath $(DESTDIR)))
 x1:=$(strip $x)
 
-generated 		:= $(bdir)/man/man8/install-docs.8	\
-						 		$(bdir)/check-stamp							\
-  					 		$(bdir)/install-docs.html				\
-  					 		$(bdir)/install-docs						
+generated     := $(bdir)/man/man8/install-docs.8  \
+                $(bdir)/check-stamp             \
+                $(bdir)/install-docs.html       \
+                $(bdir)/install-docs
 
 $(ALL_TARGET): $(generated) | $(bdir)
 
 
 
 $(bdir)/check-stamp: $(bdir)/install-docs
-	@echo; echo "*** Creating $@:"
+	$(call msg,$@)
 	PERL5LIB="perl" perl -cw $(bdir)/install-docs
 	touch $@
 
 
 $(bdir)/install-docs: install-docs.in $(CHANGELOGFILE) | $(bdir)
-	@echo; echo "*** Creating $@:"
+	$(call msg,$@)
 	sed -e '/#LINE_REMOVED_BY_MAKE#/d'   \
 	    -e 's/#VERSION#/$(VERSION)/' \
 	    < $< > $@
 	chmod 755 $@
 
 $(bdir)/man/man8/install-docs.8: $(bdir)/install-docs $(bdir)/check-stamp
-	@echo; echo "*** Creating $@:"
+	$(call msg,$@)
 	mkdir -p $(dir $@)
 	cp $< $@.pod
 	$(call podtoman,$(bdir)/man)
 
 $(bdir)/install-docs.html: $(bdir)/install-docs $(bdir)/check-stamp
-	@echo; echo "*** Creating $@:"
+	$(call msg,$@)
 	cd $(bdir) && \
 	pod2html --title "install-docs reference" \
 	  < $(notdir $<) > $(notdir $@)
@@ -50,20 +50,13 @@ define check_install
 	PERL5LIB=$(DESTDIR)/$(perldir)      perl -cw $(DESTDIR)$(sbindir)/install-docs
 endef
 
-install-local: $(generated) 
-	$(install_dir)                           $(DESTDIR)$(sbindir)
-	$(install_script) $(bdir)/install-docs   $(DESTDIR)$(sbindir)
-
-	$(install_dir)                            $(DESTDIR)$(omfdir)
-	$(install_dir)                            $(DESTDIR)$(libdir)/omf
-	$(install_dir)                            $(DESTDIR)$(libdir)/info
-	rm -f                                     $(DESTDIR)$(omfdir)/doc-base
-	$(install_link) $(libdir)/omf             $(DESTDIR)$(omfdir)/doc-base
-
-	$(install_dir)                            $(DESTDIR)$(mandir)/man8
-	$(install_file) $(bdir)/man/man8/install-docs.8  $(DESTDIR)$(mandir)/man8
-	$(compress)                               $(DESTDIR)$(mandir)/man8/install-docs.8
-
-	$(install_dir)                            $(DESTDIR)$(docdir)
-	$(install_file) $(bdir)/install-docs.html $(DESTDIR)$(docdir)
+install-local: $(generated)
+	$(call msg,$@)
+	$(call install,$(DESTDIR)$(sbindir),$(bdir)/install-docs,script)
+	$(call install,$(DESTDIR)$(omfdir),,)
+	$(call install,$(DESTDIR)$(libdir)/omf,,)
+	$(call install,$(DESTDIR)$(libdir)/info,,)
+	$(call install,$(libdir)/omf,$(DESTDIR)$(omfdir)/doc-base,link)
+	$(call install,$(DESTDIR)$(mandir)/man8,$(bdir)/man/man8/install-docs.8,compress)
+	$(call install,$(DESTDIR)$(docdir),$(bdir)/install-docs.html)
 
--- common.mk
+++ common.mk
@@ -1,51 +1,14 @@
-# vim:ts=2
+# vim:ts=2:et
 # common includes for doc-base
-# $Id: common.mk 167 2009-01-04 12:24:45Z robert $
+# $Id: common.mk 168 2009-01-04 16:10:53Z robert $
 #
 # determine our version number
 
 
 getCurrentMakefileName := $(CURDIR)/$(lastword $(MAKEFILE_LIST))
-override TOPDIR	  := $(dir $(call getCurrentMakefileName))
+override TOPDIR   := $(dir $(call getCurrentMakefileName))
 
-override PACKAGE	:= doc-base
-ifndef VERSION
-  CHANGELOGFILE     := $(TOPDIR)/debian/changelog 
-  VERSION           := $(shell LC_ALL=C dpkg-parsechangelog -l$(CHANGELOGFILE) \
-                        | sed -ne 's/^Version: *//p')
-  DATE              := $(shell LC_ALL=C dpkg-parsechangelog -l$(CHANGELOGFILE) \
-                        | sed -n 's/^Date: *//p')
-  # pretty-print the date; I wish this was dynamic like the top-level makefile but oh well
-  DATE_EN           := $(shell LC_ALL=C date --date="$(DATE)" '+%d %B, %Y')
-
-  export VERSION DATE DATE_EN
-  unexport CDPATH
-
-  ifdef DESTDIR
-    ifneq ($(DESTDIR),$(abspath $(DESTDIR)))
-      $(error DESTDIR "$(DESTDIR)" is not an absolute path)
-    endif
-  endif
-
-endif
-
-#ifdef DESTDIR
-#ifneq ($(strip $(subst /, ,$(abspath($DESTDIR))),$(subst /, ,$(abspath $(DESTDIR)))
-
-
-XGETTEXT_COMMON_OPTIONS	 := --msgid-bugs-address $(PACKAGE)@packages.debian.org	\
-														--package-name $(PACKAGE)		   											\
-														--package-version $(VERSION)												
-				
-		
-sdir            := $(CURDIR)
-ifndef bdir
-ifneq (,$(ALL_TARGET))
-bdir            := _build
-else
-bdir						:=
-endif
-endif
+override PACKAGE  := doc-base
 
 # build abstraction
 install_file    := install -p -o root -g root -m 644
@@ -63,39 +26,132 @@ perllibdir      := $(prefix)/share/perl5
 docdir          := $(prefix)/share/doc/doc-base
 libdir          := /var/lib/doc-base
 omfdir          := $(prefix)/share/omf
-nlsdir					:= $(prefix)/share/locale
+nlsdir          := $(prefix)/share/locale
+
+ifndef VERSION
+  CHANGELOGFILE     := $(TOPDIR)/debian/changelog
+  VERSION           := $(shell LC_ALL=C dpkg-parsechangelog -l$(CHANGELOGFILE) \
+                        | sed -ne 's/^Version: *//p')
+  DATE              := $(shell LC_ALL=C dpkg-parsechangelog -l$(CHANGELOGFILE) \
+                        | sed -n 's/^Date: *//p')
+  # pretty-print the date; I wish this was dynamic like the top-level makefile but oh well
+  DATE_EN           := $(shell LC_ALL=C date --date="$(DATE)" '+%d %B, %Y')
+
+  export VERSION DATE DATE_EN
+  unexport CDPATH
+
+  ifdef DESTDIR
+    ifneq ($(DESTDIR),$(abspath $(DESTDIR)))
+      $(error DESTDIR "$(DESTDIR)" is not an absolute path)
+    endif
+  endif
+endif
+
+sdir            := $(CURDIR)
+ifndef bdir
+  ifneq (,$(ALL_TARGET))
+    bdir        := _build
+  else
+    bdir        :=
+  endif
+endif
+
+ifndef DIR
+  DIR           := $(notdir $(CURDIR))
+endif
+
+XGETTEXT_COMMON_OPTIONS   := --msgid-bugs-address $(PACKAGE)@packages.debian.org  \
+                            --package-name $(PACKAGE)                           \
+                            --package-version $(VERSION)
+
+
+
+ifndef MAKE_VERBOSE
+  override MAKEFLAGS      += --silent --no-print-directory
+  define msg
+    case "$1" in                                                \
+      ""|all|all-local|build-local)                             \
+        ;;                                                      \
+      install|install-local)                                    \
+        echo "$(msgprefix) Installing files from $(DIR) ..." ;  \
+        ;;                                                      \
+      clean|clean-local)                                        \
+        echo "$(msgprefix) Cleaning $(DIR) ..."                 \
+        ;;                                                      \
+      *)                                                        \
+        echo "$(msgprefix) Making $(DIR)$(1) ..."               \
+        ;;                                                      \
+    esac
+  endef
+endif
+
+msgprefix         := *$(subst * ,*,$(wordlist 1,$(MAKELEVEL),* * * * * * * * * * * * * * * * * *))
+emptyprefix       := $(subst *, ,$(msgprefix))
+
+
+#SHELL:=/bin/echo
+# install(dir/link_target,files,mode=compress|script|link|notdir)
+define install
+  set -e;                                                           \
+  [ "$3" = "notdir" ] && dir="`dirname "$1"`" || dir="$1";          \
+  if [ -z "$2" ]; then                                              \
+    echo "$(emptyprefix) installing dir    $$dir";                  \
+    $(install_dir) "$$dir";                                         \
+  else for file in $2; do                                           \
+    [ "$3" = "notdir" ] && bfile="`basename $1`" ||                 \
+                           bfile=`basename "$$file"`;               \
+    target="$$dir/$$bfile" ;                                        \
+    [ "$3" = "link" ] || $(install_dir) "$$dir";                    \
+    if [ "$3" = "script" ] ; then                                   \
+      echo "$(emptyprefix) installing script $$target";             \
+      $(install_script) "$$file" "$$target";                        \
+    elif [ "$3" = "link" ] ; then                                   \
+      echo "$(emptyprefix) installing link   $$file";               \
+      rm -f "$$file";                                               \
+      $(install_link) "$1" "$$file";                                \
+    else                                                            \
+      echo "$(emptyprefix) installing file   $$target";             \
+      $(install_file) "$$file" "$$target";                          \
+      if [ "$3" = "compress" ]; then                                \
+        echo "$(emptyprefix) compressing file  $$target";           \
+        $(compress) "$$target";                                     \
+      fi                                                            \
+    fi                                                              \
+  done                                                              \
+  fi
+endef
+
 
 
 define pochanged
-	set -x; \
-  [ ! -e $(1) ] && rename=1 || rename=0	;	\
-	if [ $$rename = 0 ] ; then		\
-		diff=`diff -q  -I'POT-Creation-Date:' -I'PO-Revision-Date:' $(1) $(2)`; \
-		[ -z "$$diff" ] || rename=1	; \
-	fi; \
-	[ $$rename = 1 ] && mv -f $(2) $(1) || rm -f $(2); 
-  touch $(1) 
+  set -e;                                                                   \
+  [ ! -e $(1) ] && rename=1 || rename=0 ;                                   \
+  if [ $$rename = 0 ] ; then                                                \
+    diff=`diff -q  -I'POT-Creation-Date:' -I'PO-Revision-Date:' $(1) $(2)`; \
+    [ -z "$$diff" ] || rename=1 ;                                           \
+  fi;                                                                       \
+  [ $$rename = 1 ] && mv -f $(2) $(1) || rm -f $(2);                        \
+  touch $(1)
 endef
 
 define recurse
-	set -ex; \
-	for dir in $(SUBDIRS); do \
-		$(MAKE) -C $$dir $(1); \
-	done
+  set -e;                                                                 \
+  for dir in $(SUBDIRS); do                                               \
+    $(MAKE) -C $$dir DIR=$(DIR)/$$dir $(1);                               \
+  done
 endef
-# recurse := $(foreach subdir,$(SUBDIRS),$(shell $(MAKE) -C $(subdir) $1))
 
 define podtoman
-		set -ex; 																										\
-		find $1 -type f -name '*.pod' -path '*/man*' 								\
-		| while read file; do																				\
-			sed -ne '1i=encoding utf8\n' -e '/^=head1/,$$p'  < $$file \
-			| pod2man --utf8 --section=8 --center="Debian"    				\
-	  		--release="$(PACKAGE) v$(VERSION)"            					\
-	  		--date="$(DATE_EN)"                           					\
-				--name="INSTALL-DOCS"																		\
-	  	> `dirname $$file`/`basename $$file .pod`;								\
-	done
+    set -e;                                                     \
+    find $(1) -type f -name '*.pod' -path '*/man*'                \
+    | while read file; do                                       \
+      sed -ne '1i=encoding utf8\n' -e '/^=head1/,$$p'  < $$file \
+      | pod2man --utf8 --section=8 --center="Debian"            \
+        --release="$(PACKAGE) v$(VERSION)"                      \
+        --date="$(DATE_EN)"                                     \
+        --name="INSTALL-DOCS"                                   \
+      > `dirname $$file`/`basename $$file .pod`;                \
+  done
 endef
 
 
@@ -107,6 +163,7 @@ clean-local:
 install-local:
 
 clean: clean-local
+	test -z "$(bdir)" || $(call msg,$@)
 	test -z "$(bdir)" || rm -rf $(bdir)
 	$(call recurse,$@)
 
@@ -115,6 +172,7 @@ install: install-local
 	$(AFTER_INSTALL)
 
 $(bdir):
+	$(call createmsg,$@)
 	test -z "$(bdir)" || mkdir -p $(bdir)
 
 # debug
